e2ba8b8e45db3ec472effba8fd00341a44242043
Dindz <dindoquitor@gmail.com>
2025-09-14 10:33:30 +0000

    Edited in tablet

--- admin/login.php
+++ admin/login.php
@@ -41,7 +41,7 @@ $setup_complete = isset($_GET['setup']) && $_GET['setup'] === 'complete';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login - Loan Application System</title>
+    <title>Admin Login - Simple Loan Application</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
